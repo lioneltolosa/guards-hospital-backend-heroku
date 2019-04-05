98b2a275610330afdc7b2b6c0c75dd00e69eb54b
lionel <lioneltolosaarmas@gmail.com>
2019-04-05 19:58:45 +0200

    Other Test

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,36 @@ var express = require('express');
 
 var app = express();
 
+// app.get('/', (req, res, next) => {
+//     res.status(200).json({
+//         ok: true,
+//         mensaje: 'Route Working Perfectly'
+//     });
+// });
+
+var Usuario = require('../models/users');
+
+// ==========================================
+// Obtener todos los usuarios
+// ==========================================
 app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Route Working Perfectly'
-    });
+    Usuario.find({}, 'nombre email img role')
+        .exec(
+            (err, usuarios) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error cargando usuario',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    usuarios: usuarios
+                });
+            });
 });
+
 
 module.exports = app;
