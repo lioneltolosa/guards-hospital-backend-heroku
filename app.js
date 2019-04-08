4d136c9afd67b3e961c064f49eb76bfec79fb987
lionel <lioneltolosaarmas@gmail.com>
2019-04-08 17:51:19 +0200

    Clean Up Other

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // Imports Routes
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var app = express();
 
 const port = process.env.PORT || 3000;
@@ -36,17 +36,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/usuariosss', function(req, res) {
-//     res.json('get Usuario LOCAL!!!');
-// });
+app.get('/usuariosss', function(req, res) {
+    res.json('get Usuario LOCAL!!!');
+});
 
 // Conexión a la base de datos
 mongoose.connect('mongodb://localhost:27017/guardsDB', {useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 
 // Routes
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -63,63 +63,5 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-
-var Usuario = require('./models/users');
-
-// ==========================================
-// Obtener todos los usuarios
-// ==========================================
-app.get('/users', (req, res, next) => {
-    Usuario.find({}, 'nombre email img role')
-        .exec(
-            (err, usuarios) => {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        mensaje: 'Error cargando usuario',
-                        errors: err
-                    });
-                }
-
-                res.status(200).json({
-                    ok: true,
-                    usuarios: usuarios
-                });
-            });
-});
-
-// ==========================================
-// Crear un nuevo usuario
-// ==========================================
-// app.post('/users', (req, res) => {
-
-//     var body = req.body;
-
-//     var usuario = new Usuario({
-//         nombre: body.nombre,
-//         email: body.email,
-//         password: bcrypt.hashSync(body.password, 10),
-//         img: body.img,
-//         role: body.role
-//     });
-
-//     usuario.save((err, usuarioGuardado) => {
-
-//         if (err) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 mensaje: 'Error al crear usuario',
-//                 errors: err
-//             });
-//         }
-
-//         res.status(201).json({
-//             ok: true,
-//             usuario: usuarioGuardado,
-//             usuariotoken: req.usuario
-//         });
-//     });
-// });
-
 
 module.exports = app;
