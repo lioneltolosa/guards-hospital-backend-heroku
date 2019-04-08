40f08024df93b082c3f05c6f7d41087d916455ed
lionel <lioneltolosaarmas@gmail.com>
2019-04-08 17:46:09 +0200

    ADD get and post users in app.js Test

--- routes/users.js
+++ routes/users.js
@@ -1,162 +1,162 @@
-var express = require('express');
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+// var express = require('express');
+// var bcrypt = require('bcryptjs');
+// var jwt = require('jsonwebtoken');
 
-// var mdAutenticacion = require('../middlewares/autenticacion');
+// // var mdAutenticacion = require('../middlewares/autenticacion');
 
-var app = express();
+// var app = express();
 
-var Usuario = require('../models/users');
+// var Usuario = require('../models/users');
 
-// ==========================================
-// Obtener todos los usuarios
-// ==========================================
-app.get('/', (req, res, next) => {
-    Usuario.find({}, 'nombre email img role')
-        .exec(
-            (err, usuarios) => {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        mensaje: 'Error cargando usuario',
-                        errors: err
-                    });
-                }
+// // ==========================================
+// // Obtener todos los usuarios
+// // ==========================================
+// app.get('/', (req, res, next) => {
+//     Usuario.find({}, 'nombre email img role')
+//         .exec(
+//             (err, usuarios) => {
+//                 if (err) {
+//                     return res.status(500).json({
+//                         ok: false,
+//                         mensaje: 'Error cargando usuario',
+//                         errors: err
+//                     });
+//                 }
 
-                res.status(200).json({
-                    ok: true,
-                    usuarios: usuarios
-                });
-            });
-});
+//                 res.status(200).json({
+//                     ok: true,
+//                     usuarios: usuarios
+//                 });
+//             });
+// });
 
-// ==========================================
-// Actualizar usuario
-// ==========================================
-// app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
+// // ==========================================
+// // Actualizar usuario
+// // ==========================================
+// // app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 
-//     var id = req.params.id;
+// //     var id = req.params.id;
+// //     var body = req.body;
+
+// //     Usuario.findById(id, (err, usuario) => {
+
+
+// //         if (err) {
+// //             return res.status(500).json({
+// //                 ok: false,
+// //                 mensaje: 'Error al buscar usuario',
+// //                 errors: err
+// //             });
+// //         }
+
+// //         if (!usuario) {
+// //             return res.status(400).json({
+// //                 ok: false,
+// //                 mensaje: 'El usuario con el id ' + id + ' no existe',
+// //                 errors: { message: 'No existe un usuario con ese ID' }
+// //             });
+// //         }
+
+
+// //         usuario.nombre = body.nombre;
+// //         usuario.email = body.email;
+// //         usuario.role = body.role;
+
+// //         usuario.save((err, usuarioGuardado) => {
+
+// //             if (err) {
+// //                 return res.status(400).json({
+// //                     ok: false,
+// //                     mensaje: 'Error al actualizar usuario',
+// //                     errors: err
+// //                 });
+// //             }
+
+// //             usuarioGuardado.password = ':)';
+
+// //             res.status(200).json({
+// //                 ok: true,
+// //                 usuario: usuarioGuardado
+// //             });
+
+// //         });
+
+// //     });
+
+// // });
+
+
+
+// // ==========================================
+// // Crear un nuevo usuario
+// // ==========================================
+// app.post('/', (req, res) => {
+
 //     var body = req.body;
 
-//     Usuario.findById(id, (err, usuario) => {
-
-
-//         if (err) {
-//             return res.status(500).json({
-//                 ok: false,
-//                 mensaje: 'Error al buscar usuario',
-//                 errors: err
-//             });
-//         }
-
-//         if (!usuario) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 mensaje: 'El usuario con el id ' + id + ' no existe',
-//                 errors: { message: 'No existe un usuario con ese ID' }
-//             });
-//         }
-
-
-//         usuario.nombre = body.nombre;
-//         usuario.email = body.email;
-//         usuario.role = body.role;
-
-//         usuario.save((err, usuarioGuardado) => {
-
-//             if (err) {
-//                 return res.status(400).json({
-//                     ok: false,
-//                     mensaje: 'Error al actualizar usuario',
-//                     errors: err
-//                 });
-//             }
-
-//             usuarioGuardado.password = ':)';
-
-//             res.status(200).json({
-//                 ok: true,
-//                 usuario: usuarioGuardado
-//             });
-
-//         });
-
+//     var usuario = new Usuario({
+//         nombre: body.nombre,
+//         email: body.email,
+//         password: bcrypt.hashSync(body.password, 10),
+//         img: body.img,
+//         role: body.role
 //     });
 
-// });
-
-
-
-// ==========================================
-// Crear un nuevo usuario
-// ==========================================
-app.post('/', (req, res) => {
-
-    var body = req.body;
-
-    var usuario = new Usuario({
-        nombre: body.nombre,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-        img: body.img,
-        role: body.role
-    });
-
-    usuario.save((err, usuarioGuardado) => {
-
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Error al crear usuario',
-                errors: err
-            });
-        }
-
-        res.status(201).json({
-            ok: true,
-            usuario: usuarioGuardado,
-            usuariotoken: req.usuario
-        });
-    });
-});
-
-
-// ============================================
-//   Borrar un usuario por el id
-// ============================================
-// app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
-
-//     var id = req.params.id;
-
-//     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+//     usuario.save((err, usuarioGuardado) => {
 
 //         if (err) {
-//             return res.status(500).json({
+//             return res.status(400).json({
 //                 ok: false,
-//                 mensaje: 'Error borrar usuario',
+//                 mensaje: 'Error al crear usuario',
 //                 errors: err
 //             });
 //         }
 
-//         if (!usuarioBorrado) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 mensaje: 'No existe un usuario con ese id',
-//                 errors: { message: 'No existe un usuario con ese id' }
-//             });
-//         }
-
-//         res.status(200).json({
+//         res.status(201).json({
 //             ok: true,
-//             usuario: usuarioBorrado
+//             usuario: usuarioGuardado,
+//             usuariotoken: req.usuario
 //         });
-
 //     });
-
 // });
 
 
-module.exports = app;
+// // ============================================
+// //   Borrar un usuario por el id
+// // ============================================
+// // app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+// //     var id = req.params.id;
+
+// //     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+// //         if (err) {
+// //             return res.status(500).json({
+// //                 ok: false,
+// //                 mensaje: 'Error borrar usuario',
+// //                 errors: err
+// //             });
+// //         }
+
+// //         if (!usuarioBorrado) {
+// //             return res.status(400).json({
+// //                 ok: false,
+// //                 mensaje: 'No existe un usuario con ese id',
+// //                 errors: { message: 'No existe un usuario con ese id' }
+// //             });
+// //         }
+
+// //         res.status(200).json({
+// //             ok: true,
+// //             usuario: usuarioBorrado
+// //         });
+
+// //     });
+
+// // });
+
+
+// module.exports = app;
 
 
 
@@ -170,11 +170,11 @@ module.exports = app;
 
 
 
-// var router = express.Router();
+// // var router = express.Router();
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+// // /* GET users listing. */
+// // router.get('/', function(req, res, next) {
+// //   res.send('respond with a resource');
+// // });
 
-// module.exports = router;
+// // module.exports = router;
