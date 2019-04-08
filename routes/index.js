d39485677bcae8f7cd547163c776a0f91532b53e
lionel <lioneltolosaarmas@gmail.com>
2019-04-08 20:43:38 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,35 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Route Working Perfectly'
-    });
+var Usuario = require('../models/users');
+
+// app.get('/', (req, res, next) => {
+//     res.status(200).json({
+//         ok: true,
+//         mensaje: 'Route Working Perfectly'
+//     });
+// });
+
+app.get('/users', (req, res, next) => {
+    
+    // res.sendFile(path.resolve(__dirname + '/public/index.html'));
+
+    Usuario.find({}, 'nombre email img role')
+        .exec(
+            (err, usuarios) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error cargando usuario',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    usuarios: usuarios
+                });
+            });
 });
 
 module.exports = app;
