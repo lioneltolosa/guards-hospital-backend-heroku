ef7a28067a162fe44aaf44a8b3374a080a59e19e
lionel <lioneltolosaarmas@gmail.com>
2019-04-08 16:15:16 +0200

    Clean Up

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,11 @@ var express = require('express');
 
 var app = express();
 
-// app.get('/', (req, res, next) => {
-//     res.status(200).json({
-//         ok: true,
-//         mensaje: 'Route Working Perfectly'
-//     });
-// });
-
-var Usuario = require('../models/users');
-
-// ==========================================
-// Obtener todos los usuarios
-// ==========================================
 app.get('/', (req, res, next) => {
-    Usuario.find({}, 'nombre email img role')
-        .exec(
-            (err, usuarios) => {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        mensaje: 'Error cargando usuario',
-                        errors: err
-                    });
-                }
-
-                res.status(200).json({
-                    ok: true,
-                    usuarios: usuarios
-                });
-            });
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Route Working Perfectly'
+    });
 });
-
 
 module.exports = app;
